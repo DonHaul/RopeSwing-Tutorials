2d43d6fd59bbaa2d017091ea6ad448782881729b
Ramiro <ramiro.animus@gmai.com>
2016-09-10 13:59:39 +0100

    Part 1 and 2 added

---
+++ Part 1/RopeScript.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class RopeScript : MonoBehaviour {
+
+	public Vector2 destiny;
+
+	public float speed= 1;
+
+
+	public float distance = 2;
+
+	public GameObject nodePrefab;
+
+	public GameObject player;
+
+	public GameObject lastNode;
+
+	bool done=false;
+
+	// Use this for initialization
+	void Start () {
+
+		player = GameObject.FindGameObjectWithTag ("Player");
+
+		lastNode = transform.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		transform.position = Vector2.MoveTowards (transform.position,destiny,speed);
+
+
+
+
+		if ((Vector2)transform.position != destiny) {
+
+			if (Vector2.Distance (player.transform.position, lastNode.transform.position) > distance) {
+
+
+				CreateNode ();
+
+			}
+
+
+		} else if (done == false) {
+
+			done = true;
+
+
+			lastNode.GetComponent<HingeJoint2D> ().connectedBody = player.GetComponent<Rigidbody2D> ();
+		}
+
+
+		
+	}
+
+
+	void CreateNode()
+	{
+
+		Vector2 pos2Create = player.transform.position - lastNode.transform.position;
+		pos2Create.Normalize ();
+		pos2Create *= distance;
+		pos2Create += (Vector2)lastNode.transform.position;
+
+		GameObject go = (GameObject)	Instantiate (nodePrefab, pos2Create, Quaternion.identity);
+
+
+		go.transform.SetParent (transform);
+
+		lastNode.GetComponent<HingeJoint2D> ().connectedBody = go.GetComponent<Rigidbody2D> ();
+
+		lastNode = go;
+
+
+	}
+
+
+
+}
---
+++ Part 2/throwhook.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class throwhook : MonoBehaviour {
+
+
+	public GameObject hook;
+
+	GameObject curHook;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+
+		if (Input.GetMouseButtonDown (0)) {
+
+				Vector2 destiny = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+
+
+				curHook = (GameObject)Instantiate (hook, transform.position, Quaternion.identity);
+
+				curHook.GetComponent<RopeScript> ().destiny = destiny;
+
+			
+		}
+	}
+}
