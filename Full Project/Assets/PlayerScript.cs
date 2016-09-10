98abf7a38b40de79bbe8b32141ebd356ec79622b
Ramiro <ramiro.animus@gmai.com>
2016-09-10 13:58:39 +0100

    full project added

---
+++ Full Project/Assets/PlayerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+
+	public float forcetoAdd=100;
+
+
+
+
+
+	void Start () {
+		//gives it force
+		GetComponent<Rigidbody2D> ().velocity = Vector2.up * 10;
+
+	}
+
+
+	void Update () {
+
+		if (Input.GetKey (KeyCode.A))  
+			GetComponent<Rigidbody2D> ().AddForce(-Vector2.right*forcetoAdd);
+
+		if (Input.GetKey (KeyCode.D))  
+			GetComponent<Rigidbody2D> ().AddForce(Vector2.right*forcetoAdd);
+	}			
+}
---
+++ Full Project/Assets/throwhook.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class throwhook : MonoBehaviour {
+
+
+	public GameObject hook;
+
+
+	public bool ropeActive;
+
+	GameObject curHook;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+
+		if (Input.GetMouseButtonDown (0)) {
+
+
+			if (ropeActive == false) {
+				Vector2 destiny = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+
+
+				curHook = (GameObject)Instantiate (hook, transform.position, Quaternion.identity);
+
+				curHook.GetComponent<RopeScript> ().destiny = destiny;
+
+
+				ropeActive = true;
+			} else {
+
+				//delete rope
+
+				Destroy (curHook);
+
+
+				ropeActive = false;
+
+			}
+		}
+
+
+	}
+}
